6741c9abfa0a64dcba4871d39aed5df75944a54d
jonahgcarpenter <jonahgcarpenter@gmail.com>
2025-03-04 12:26:57 -0600

    connected mongodb along with a more advanced server entry

--- server/server.js
+++ server/server.js
@@ -1,11 +1,48 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+// Create the app
 const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Console log requested path and method
+app.use((req, _res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// Serve the static files from Vite
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+// Default port
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// Ensure the MongoDB URI is defined
+const mongoURI = process.env.MONGODB_URI;
+if (!mongoURI) {
+  console.error(
+    "Error: MONGODB_URI is not defined in your environment variables.",
+  );
+  process.exit(1);
+}
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Connect database and start server
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Connected to database, server listening on port:", port);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error);
+  });
